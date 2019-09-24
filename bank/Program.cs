2b24e5bbeb016cdb08cbd4bff8fd4bf84d1beb6c
Antonio Almeida <antonio.castro@projectual-ma.com.br>
2019-09-24 16:56:24 -0300

    Atividade de Exceptions

--- bank/Program.cs
+++ bank/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using bank.Entities;
+using bank.Entities.Exceptions;
+using System.Globalization;
 
 namespace bank
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+                Console.WriteLine("Enter account data");
+                Console.Write("Number:");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder:");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance:");
+                double balance = double.Parse(Console.ReadLine());
+                Console.Write("withdraw limit:");
+                double withdrawlimit = double.Parse(Console.ReadLine());
+                Account account = new Account(number, holder, balance, withdrawlimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw:");
+                double amount = double.Parse(Console.ReadLine());
+            try
+            {
+                account.WithDraw(amount);
+                Console.WriteLine("New balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+
         }
     }
 }
